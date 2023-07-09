b23a57dd67e4da3b0b3130ee2bf111e151c0335b
lsculv <112150142+lsculv@users.noreply.github.com>
2023-07-08 22:35:05 -0700

    Add support for subcommands that operate on whole files
    Makes the `sum` and `unique` commands avaliable

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::str::FromStr;
 
 use clap::{Arg, Command};
 use colored::Colorize;
+use itertools::Itertools;
 
 #[derive(Debug)]
 enum IterTool {
@@ -13,17 +14,38 @@ enum IterTool {
     TrimEnd,
     ToUppercase,
     ToLowercase,
+    Unique,
+    Sum,
 }
 
 impl IterTool {
-    pub fn apply_to<'a>(&'a self, buf: &'a str) -> String {
+    pub fn apply_to<'a>(&'a self, buf: &'a str) -> anyhow::Result<String> {
         match self {
-            IterTool::Trim => buf.trim().to_string(),
-            IterTool::TrimStart => buf.trim_start().to_string(),
-            IterTool::TrimEnd => buf.trim_end().to_string(),
-            IterTool::ToUppercase => buf.to_uppercase(),
-            IterTool::ToLowercase => buf.to_lowercase(),
+            IterTool::Trim => Ok(buf.trim().to_string()),
+            IterTool::TrimStart => Ok(buf.trim_start().to_string()),
+            IterTool::TrimEnd => Ok(buf.trim_end().to_string()),
+            IterTool::ToUppercase => Ok(buf.to_uppercase()),
+            IterTool::ToLowercase => Ok(buf.to_lowercase()),
+            IterTool::Unique => Ok(buf.lines().unique().join("\n")),
+            IterTool::Sum => {
+                let mut sum: i64 = 0;
+                for line in buf.lines() {
+                    sum += line.parse::<i64>()?;
+                }
+                Ok(sum.to_string())
+            }
         }
+    }
+
+    pub fn is_by_lines(&self) -> bool {
+        matches!(
+            self,
+            IterTool::Trim
+                | IterTool::TrimStart
+                | IterTool::TrimEnd
+                | IterTool::ToUppercase
+                | IterTool::ToLowercase
+        )
     }
 
     pub fn removes_newline(&self) -> bool {
@@ -41,6 +63,10 @@ impl FromStr for IterTool {
             "trim_end" => Ok(IterTool::TrimEnd),
             "to_uppercase" => Ok(IterTool::ToUppercase),
             "to_lowercase" => Ok(IterTool::ToLowercase),
+            "unique" => Ok(IterTool::Unique),
+            "sum" => Ok(IterTool::Sum),
+            // This Err should never be reached in a release version as clap
+            // should exclude invalid subcommands at the args parsing step
             _ => Err(()),
         }
     }
@@ -57,30 +83,58 @@ fn run(config: Config) -> anyhow::Result<()> {
 
     let mut readers: Vec<Box<dyn BufRead>> = Vec::with_capacity(config.files.len());
     for file in config.files {
-        readers.push(make_reader(&file)?);
+        match make_reader(&file) {
+            Ok(reader) => readers.push(reader),
+            // Continue on file read error
+            Err(e) => eprintln!("it: '{}': {}", file, e),
+        }
     }
 
     let command = config.command;
 
-    let mut buf_stdout = BufWriter::new(stdout().lock());
-    let mut buf = String::with_capacity(255);
+    if command.is_by_lines() {
+        apply_by_lines(readers, command)?;
+    } else {
+        apply_to_entire(readers, command)?;
+    }
 
-    for mut reader in readers {
-        loop {
-            let bytes = reader.read_line(&mut buf)?;
-            if bytes == 0 {
+    #[inline(always)]
+    fn apply_by_lines(readers: Vec<Box<dyn BufRead>>, command: IterTool) -> anyhow::Result<()> {
+        let mut buf_stdout = BufWriter::new(stdout().lock());
+        let mut buf = String::with_capacity(255);
+        for mut reader in readers {
+            loop {
+                let bytes = reader.read_line(&mut buf)?;
+                if bytes == 0 {
+                    buf.clear();
+                    break;
+                }
+                write!(buf_stdout, "{}", command.apply_to(&buf)?)?;
+                if command.removes_newline() {
+                    writeln!(buf_stdout)?;
+                }
+
                 buf.clear();
-                break;
             }
-            write!(buf_stdout, "{}", command.apply_to(&buf))?;
-            if command.removes_newline() {
-                writeln!(buf_stdout)?;
-            }
+        }
+        buf_stdout.flush()?;
 
+        Ok(())
+    }
+
+    #[inline(always)]
+    fn apply_to_entire(readers: Vec<Box<dyn BufRead>>, command: IterTool) -> anyhow::Result<()> {
+        let mut buf_stdout = BufWriter::new(stdout().lock());
+        let mut buf = String::new();
+        for mut reader in readers {
+            reader.read_to_string(&mut buf)?;
+            writeln!(buf_stdout, "{}", command.apply_to(&buf)?)?;
             buf.clear();
         }
+        buf_stdout.flush()?;
+
+        Ok(())
     }
-    buf_stdout.flush()?;
 
     Ok(())
 }
@@ -106,10 +160,12 @@ fn parse_args() -> anyhow::Result<Config> {
                 .global(true),
         )
         .subcommand(Command::new("trim"))
-        .subcommand(Command::new("trim_start").visible_alias("trim_left"))
-        .subcommand(Command::new("trim_end").visible_alias("trim_right"))
+        .subcommand(Command::new("trim_start").visible_aliases(["trim_left", "triml"]))
+        .subcommand(Command::new("trim_end").visible_aliases(["trim_right", "trimr"]))
         .subcommand(Command::new("to_uppercase").visible_aliases(["upper", "uppercase"]))
         .subcommand(Command::new("to_lowercase").visible_aliases(["lower", "lowercase"]))
+        .subcommand(Command::new("unique").visible_alias("uniq"))
+        .subcommand(Command::new("sum"))
         .get_matches();
 
     let (subcommand, subargs) = matches
@@ -130,7 +186,7 @@ fn parse_args() -> anyhow::Result<Config> {
 
 fn main() {
     if let Err(e) = parse_args().and_then(run) {
-        eprintln!("{} {}", "error:".bright_red(), e);
+        eprintln!("{} {}", "error:".bright_red().bold(), e);
         process::exit(1);
     }
 }
